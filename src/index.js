6825eaab70bc6d4006752ef51888a1295bd55f9e
Andrew Kos <akos123@gmail.com>
2016-06-04 09:14:39 -0500

    basics

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let express = require('express');
+let expressHandlebars = require('express-handlebars');
+let path = require('path');
+
+let app = express();
+
+const APP_PORT = 8080;
+const APP_HOSTNAME = 'localhost';
+const DIR_TEMPLATES = path.join('.', 'src', 'templates');
+
+app.engine('hbs', expressHandlebars({
+  defaultLayout: 'index',
+  extname: 'hbs',
+  layoutsDir: DIR_TEMPLATES
+}));
+app.set('view engine', 'hbs');
+app.set('views', DIR_TEMPLATES);
+
+app.use('/static', express.static('public'));
+app.use('', require('./routes'));
+
+app.listen(APP_PORT, () => {
+  console.log(`Server at ${APP_HOSTNAME}:${APP_PORT}`);
+});
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('index', {});
+});
+
+module.exports = router;
