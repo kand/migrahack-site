d3241fd5d151774f40d31d9bf1dc9064e9b9dea9
Andrew Kos <akos123@gmail.com>
2016-06-05 00:26:03 -0500

    remove font-awesome

--- src/bundle.js
+++ src/bundle.js
@@ -1,10 +1,5 @@
 'use strict';
 
-let fonts = require.context('font-awesome/fonts', true, /.*\.(eot|svg|tff|woff2?)$/);
-fonts.keys().map(fonts);
-
-require('font-awesome/scss/font-awesome.scss');
-
 require('./app/main.scss');
 
 require('./app/client.js');
